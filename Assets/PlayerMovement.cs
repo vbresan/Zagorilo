c5cb904cb6641e86b68472a73a92187ed498ea9b
Viktor Bresan <288855+vbresan@users.noreply.github.com>
2025-02-04 08:07:56 +0100

    Implementing player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,42 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 3f;
     [SerializeField] private float rotationSpeed = 15f;
 
+    private bool IsNotCollision(Vector3 direction) {
+
+        float playerHeight = 2f;
+        float playerRadius = 0.3f;
+        float distance     = movementSpeed * Time.deltaTime;
+
+        return !Physics.CapsuleCast(
+            transform.position, 
+            transform.position + Vector3.up * playerHeight, 
+            playerRadius, 
+            direction, 
+            distance
+        );
+    }
+
+    private bool CanMoveInDirection(ref Vector3 direction) {
+
+        if (IsNotCollision(direction)) {
+            return true;
+        }
+
+        Vector3 directionX = new Vector3(direction.x, 0, 0).normalized;
+        if (IsNotCollision(directionX)) {
+            direction = directionX;
+            return true;
+        }
+
+        Vector3 directionZ = new Vector3(0, 0, direction.z).normalized;
+        if (IsNotCollision(directionZ)) {
+            direction = directionZ;
+            return true;
+        }        
+
+        return false;
+    }
+
     void Start() {
         inputSystem = new InputSystem();
         inputSystem.Player.Enable();
@@ -17,15 +53,21 @@ public class PlayerMovement : MonoBehaviour
     void Update() {
         
         Vector2 inputVector = GetInputVector();
-        Vector3 movement    = new Vector3(inputVector.x, 0, inputVector.y);
+        Vector3 direction   = new Vector3(inputVector.x, 0, inputVector.y);
 
-        transform.position += movement * movementSpeed * Time.deltaTime;
-
-        if (movement != Vector3.zero) {
-            transform.forward = Vector3.Slerp(transform.forward, movement, rotationSpeed * Time.deltaTime);
+        if (CanMoveInDirection(ref direction)) {
+            transform.position += direction * movementSpeed * Time.deltaTime;
         }
 
-        isWalking = movement != Vector3.zero;
+        if (direction != Vector3.zero) {
+            transform.forward = Vector3.Slerp(
+                transform.forward, 
+                direction, 
+                rotationSpeed * Time.deltaTime
+            );
+        }
+
+        isWalking = direction != Vector3.zero;
     }    
 
     public Vector2 GetInputVector() {
